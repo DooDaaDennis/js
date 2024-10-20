fe26c3d998f2dc2df7fa7cb315cde3a8f7c9fe60
DooDaaDennis <dennis.applebaum@btinternet.com>
2024-10-20 18:01:11 +0100

    objects section

--- Section-3/index.js
+++ Section-3/index.js
@@ -1,19 +1,80 @@
-var x = 10;
-const y = true;
-var anotherObject;
+/* let myObject;
+
+myObject = {
+  a: 10,
+  b: "abc"
+}
+
+console.log(myObject)
+
+myObject.a = 15;
+
+console.log(myObject)
+
+myObject.c = true
+
+console.log(myObject)
+
+delete myObject.b
+
+console.log(myObject)
+
+console.log(myObject.a) */
+
+// var myPost = {}
+
+// myPost.title = "Object is reference type"
+// myPost.postLikes = 0
+// myPost.shared = false
+
+// myPost.postLikes += 1
+
+// delete myPost.shared
+
+// console.log(myPost)
+
+// const myObject = {
+//   a: -10
+// };
+
+// var copyOfMyObject = myObject;
+
+// copyOfMyObject.b = false;
+
+// console.log(myObject);
+// console.log(copyOfMyObject);
+
+// let myObject = {
+//   a: true,
+//   b: null,
+//   c: 25
+// };
+
+// console.log(myObject["a"]);
+// console.log(myObject["b"]);
+
+// const propertyName = "c";
+
+// console.log(myObject[propertyName]);
 
 const myObject = {
-  a: x,
-  b: y,
+  a: 3,
+  b: true
 };
 
-x = 20;
+// myObject.newPropertyWithUndefinedValue = undefined //use null intead
 
-anotherObject = {
-  newA: x,
-  b: y,
-  c: myObject,
+// console.log(myObject.newPropertyWithUndefinedValue)
+
+var objectWithNestedObject = {
+
 };
 
-console.log(myObject);
-console.log(anotherObject);
+objectWithNestedObject.nestedObject = {
+
+};
+
+objectWithNestedObject.nestedObject.a = null
+objectWithNestedObject.nestedObject["b"] = true
+
+console.log(objectWithNestedObject);
