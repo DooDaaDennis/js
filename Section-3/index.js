15fad7cf4862f467c17e05c146d60f92da2c95fb
DooDaaDennis <dennis.applebaum@btinternet.com>
2024-10-21 00:20:10 +0100

    logic

--- Section-3/index.js
+++ Section-3/index.js
@@ -1,45 +1,161 @@
-// const myArray = [1,2,3];
-// const myArray2 = new Array(1,2,3);
+// FOR
 
-// console.log(myArray === myArray2);
-// console.log(myArray == myArray2)
+// for(let i = 0; i < 4; i++){
+//     console.log(i);
+// }
 
-// const myArray = [true, null];
-// myArray.unshift("Hello");
-// myArray.unshift(100);
+// const myArray = ["first", "second", "third"];
+
+// for (let i = 0; i < myArray.length; i++){
+//     console.log(myArray[i]);
+// }
 
 
-// console.log(myArray);
+// WHILE
+// let i = 0;
 
-// const myArray = [1,2];
+// while (i < 5){
+//     console.log(i);
+//     i++;
+// }
 
-// myArray[10] = "abc";
+// DO WHILE
+// let i = 0
+// do {
+//     console.log(i);
+//     i++;
+// } while (i<5);
 
-// console.log(myArray);
-// console.log(myArray.length);
+// FOR IN
 
-const car1 = {
-    carBrand: "Mercedes",
-    price: 20000
+// const myObject = {
+//     x: 10,
+//     y: true,
+//     z: "abc"
+// }
+
+// // for (let key in myObject){
+// //     console.log(key, myObject[key]);
+// // }
+
+// const myArray = [true, 10, "abc", null];
+
+// // for (let key in myArray){
+// //     console.log(myArray[key]);
+// // }
+
+// // FOR OF - not with objects, not iterable
+
+// for (let element of myArray){
+//     console.log(element);
+// }
+
+// IF
+// let val = 10;
+// if (val > 5){
+//     val += 20;
+// }
+
+// console.log(val);
+
+// IF ELSE
+// let val = 10
+// if(val < 5){
+//     val += 20;
+// } else{
+//     val -= 20;
+// }
+// console.log(val);
+
+// SWITCH
+// let month = 2;
+// switch (month){
+//     case 1:
+//         console.log("It is January");
+//         break;
+//     case 2:
+//         console.log("It is February");
+//         break;
+//     default:
+//         console.log("It's some other month");
+// }
+
+// TERNARY OPERATOR
+
+//Condition ? Expression1 : Expression2`
+
+// let value = 12;
+
+// value
+//     ? console.log("Value is truthy")
+//     : console.log("Value is falsy");
+
+// let value1 = 11;
+// let value2 = 25;
+
+// value1 && value2    
+//     ? myFunction1(value1, value2)
+//     : myFunction2();
+
+
+//challenge 1
+
+// const myObject ={
+//     key1: true,
+//     key5: 10, 
+//     key3: "abc",
+//     key4: null,
+//     key10: NaN
+// }
+
+// for (let key in myObject){
+    
+//     switch  (key){
+//         case "key1":
+//             console.log(myObject[key]);
+//             break;
+//         case "key3":
+//             console.log(myObject[key]);
+//             break;
+//         default:
+//     }}
+
+//challenge 2
+const MIN = 1000;
+const MAX = 9999;
+const myNumbers = [
+    2355,
+    7235,
+    8135,
+    1762,
+    2361,
+    8351
+];
+
+let newRandomNumber;
+
+let l = myNumbers.length
+
+function getRandomInt(min, max){
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-const car2 = {
-    carBrand: "Honda",
-    price: 15000
+function isNotUnique(newRandomNum){
+    for (let number of myNumbers){
+        if (newRandomNum === number){
+            return true;
+        }
+    }
 }
 
-const car3 = {
-    carBrand: "Ford",
-    price: 18000
-}
 
-const car4 = {
-    carBrand: "Jaguar",
-    price: 40000
-}
+do { 
+    newRandomNumber = getRandomInt(MIN,MAX);
+    console.log(newRandomNumber);
 
-const arrayOfCars = [car1, car2, car3];
+} while (isNotUnique(newRandomNumber));
 
-arrayOfCars.push(car4);
-
-console.log(arrayOfCars);
+myNumbers.push(newRandomNumber);
+console.log(myNumbers)
