b550b7b5bab39f94261bf98aa175e5daee43db65
DooDaaDennis <dennis.applebaum@btinternet.com>
2024-10-20 12:55:14 +0100

    Section 4

--- Section-3/index.js
+++ Section-3/index.js
@@ -1,1 +1,19 @@
-console.log("Hello from external JS file");
+var x = 10;
+const y = true;
+var anotherObject;
+
+const myObject = {
+  a: x,
+  b: y,
+};
+
+x = 20;
+
+anotherObject = {
+  newA: x,
+  b: y,
+  c: myObject,
+};
+
+console.log(myObject);
+console.log(anotherObject);
